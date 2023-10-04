ee34e9e670ec91e7bfe5e31dc1d02a339713ce85
JordiMateoUdL <jordi.mateo@udl.cat>
2023-10-04 11:34:41 +0200

    feat: add sources

--- src/pokemon.c
+++ src/pokemon.c
@@ -7,24 +7,37 @@
 #include <string.h> //strlen(), strcpy()
 #include "pokemon.h"
 
-Pokemon create_pokemon(int pokemon_id, char *name, double height, double weight) {
-    Pokemon pokemon;
-    pokemon.pokemon_id = pokemon_id;
-    pokemon.name = malloc( (strlen(name) +1) * sizeof(char));
-    strcpy(pokemon.name, name);
-    pokemon.height = height;
-    pokemon.weight = weight;
+struct pokemon {
+    int          pokemon_id;
+    char *       name;
+    double       height;
+    double       weight;
+};
+
+Pokemon* create_pokemon(int pokemon_id, char *name, double height, double weight) {
+    Pokemon* pokemon;
+    pokemon = malloc(sizeof(Pokemon));
+    pokemon->pokemon_id = pokemon_id;
+    pokemon->name = malloc( (strlen(name) +1) * sizeof(char));
+    strcpy(pokemon->name, name);
+    pokemon->height = height;
+    pokemon->weight = weight;
 
     return pokemon;
 }
 
-void print_pokemon(Pokemon pokemon) {
-    printf("Pokemon: %s\n", pokemon.name);
-    printf("Pokemon ID: %d\n", pokemon.pokemon_id);
-    printf("Pokemon Height: %f\n", pokemon.height);
-    printf("Pokemon Weight: %f\n", pokemon.weight);
+void print_pokemon(Pokemon* pokemon) {
+    printf("Pokemon: %s\n", pokemon->name);
+    printf("Pokemon ID: %d\n", pokemon->pokemon_id);
+    printf("Pokemon Height: %f\n", pokemon->height);
+    printf("Pokemon Weight: %f\n", pokemon->weight);
 }
 
-void destroy_pokemon(Pokemon pokemon) {
-    free(pokemon.name);
+void destroy_pokemon(Pokemon* pokemon) {
+    free(pokemon->name);
+    free(pokemon);
+}
+
+void set_pokemon_id(Pokemon* pokemon, int id){
+    pokemon->pokemon_id = id;
 }
--- src/pokemon.h
+++ src/pokemon.h
@@ -5,15 +5,11 @@
 #ifndef _POKEMON_H_
 #define _POKEMON_H_
 
-typedef struct pokemon {
-    int          pokemon_id;
-    char *       name;
-    double       height;
-    double       weight;
-} Pokemon;
+typedef struct pokemon Pokemon;
 
-Pokemon create_pokemon(int pokemon_id, char *name, double height, double weight);
-void print_pokemon(Pokemon pokemon);
-void destroy_pokemon(Pokemon pokemon);
+Pokemon* create_pokemon(int pokemon_id, char *name, double height, double weight);
+void print_pokemon(Pokemon* pokemon);
+void destroy_pokemon(Pokemon* pokemon);
+void set_pokemon_id(Pokemon* , int id);
 
 #endif // _POKEMON_H_
